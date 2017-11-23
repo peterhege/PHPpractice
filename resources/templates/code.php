b529ab8b017e15e8d863b16e377c6fb67ed210dc
peterhege <mr.peter.hege@gmail.com>
2017-11-23 19:33:30 +0100

    test codes

--- resources/templates/code.php
+++ resources/templates/code.php
@@ -1,15 +1,33 @@
 <?php
-$code = file_get_contents(TEMPLATES_PATH.'/'.$segments[1].'.php');
+$file = $segments[1];
+$code = file_get_contents(TEMPLATES_PATH.'/'.$file.'.php');
+$test_code = file_get_contents(TEMPLATES_PATH.'/examples/'.$file.'.txt');
+include $file.'.php';
 ?>
 
 <div class="container">
-    <h1><?=$segments[1]?></h1>
+    <h1><?=$file?></h1>
     <div class="code-container">
         <?=highlight_string($code,true);?>
     </div>
-</div>
 
-<form action="" method="get">
-    <input name="hello" type="text">
-    <button>Submit</button>
-</form>
+
+    <h1>Test</h1>
+    <form action="" method="post" class="my-form">
+        <div class="form-group">
+            <textarea name="code" class="form-control" id="exampleTextarea" rows="3"><?php
+                if(isset($_POST['code']))
+                    echo $_POST['code'];
+                else
+                    echo $test_code;
+            ?></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Test</button>
+    </form>
+    <?php if(isset($_POST['code'])){ ?>
+    <h1>Output</h1>
+    <div class="code-container">
+        <?=eval($_POST['code']);?>
+    </div>
+    <?php } ?>
+</div>
